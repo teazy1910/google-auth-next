9a5b2329f85ed097b34278f26e7e9d9ebe7c9485
tanja dietz <tanjaisabel.dietz@gmail.com>
2023-10-09 08:18:21 +0200

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+import SigninButton from './SigninButton'
+
+export const Navbar = () => {
+  return (
+   <header className='flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow'>
+        <SigninButton/>
+    </header>
+  )
+}
+export default Navbar
